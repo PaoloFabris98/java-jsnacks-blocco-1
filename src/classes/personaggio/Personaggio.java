0e99c87f2fe5a967ca845bb318dcfb7f1dc61ab2
PaoloFabris98 <f98paolo@gmail.com>
2025-01-28 19:22:31 +0100

    feat: stats

--- src/classes/personaggio/Personaggio.java
+++ src/classes/personaggio/Personaggio.java
@@ -3,7 +3,21 @@ package classes.personaggio;
 public class Personaggio {
     private int PF = 0;
     private int MP = 0;
+    private int attacco = 0;
+    private int difesa = 0;
 
-    public Personaggio() {
+    public Personaggio(int PF, int MP, int attacco, int difesa) {
+        if (PF <= 0) {
+            throw new IllegalArgumentException("I punti vita non possono essere inizializzati come 0.");
+        }
+        if (PF < 0) {
+            throw new IllegalArgumentException("I mana point non possono essere inferiori a 0.");
+        }
+        if (attacco < 0) {
+            throw new IllegalArgumentException("l'attacco non può essere inizializzato come 0");
+        }
+        if (difesa < 0) {
+            throw new IllegalArgumentException("la difesa non può essere inizializzata come 0");
+        }
     }
 }
